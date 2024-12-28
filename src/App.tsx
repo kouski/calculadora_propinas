0a11531acec7d595421243de1ea8846ca753013a
kouski <72550619+kouski@users.noreply.github.com>
2024-12-29 00:16:56 +0100

    primer componente MenuItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {menuItems} from './data/db';
+import MenuItem from './components/MenuItem';
 
 
 function App() {
@@ -9,6 +10,24 @@ function App() {
       <header className=" bg-teal-400 py-5 ">
         <h1 className=" text-center text-2xl font-black">Calculadora de propinas</h1>
       </header>
+
+      <main className=' max-w-7xl mx-auto px-20 py-20 grid md:grid-cols-2'>
+        <div>
+          <h2>Menú</h2>
+          {menuItems.map(item =>(
+            <MenuItem
+              key={item.id}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
+        </div>
+        
+        <div>
+          <h2>Consumo</h2>
+        </div>
+        
+      </main>
       
     </>
   )
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,10 @@
+
+
+export default function MenuItem(props:any) {
+  return (
+    <div>
+        <p className="bg-orange-300">{props.name} {props.price} €</p>
+        
+    </div>
+  )
+}

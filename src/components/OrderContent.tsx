d44607ed376018d8abe316db2156988a79ba7bc6
kouski <72550619+kouski@users.noreply.github.com>
2024-12-29 23:18:24 +0100

    contenido orden y boton eliminar

--- src/components/OrderContent.tsx
+++ src/components/OrderContent.tsx
@@ -5,6 +5,9 @@ type OrdenContentProps = {
 }
 
 export default function OrderContent({order}: OrdenContentProps) {
+
+
+
   return (
     <div>
         <h2 className=' text-2xl font-black mb-6'>Consumo</h2>
@@ -12,9 +15,15 @@ export default function OrderContent({order}: OrdenContentProps) {
             {order.length === 0? 
             <p className=" text-center">La orden está vacía</p>  
             : (order.map(item=>(
-                <div key={item.id} className=" flex justify-between">
+                <div key={item.id} className=" flex justify-between items-center border-t border-gray-200 py-1 last-of-type:border-b">
+                    <div>
                     <p className=" text-lg">{item.name} - {item.price} €</p>
-                    <p className=" font-black">Cantidad: {item.quantity}</p>
+                    <p className=" font-black">Cantidad:  {item.quantity} - {item.price * item.quantity} €</p>
+                </div>
+
+                    <button className="bg-red-600 h-6 w-6 font-black text-white rounded-full">x</button>
+                    
+                    
                 </div>
             ))) }
         </div>
